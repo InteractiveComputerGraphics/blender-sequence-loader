c6c5886288a0f6bc8b5ab487f7ef83e93aa90f0e
Hantao Hui <hantao.hui@rwth-aachen.de>
2021-06-27 15:18:56 +0200

    minor change

--- meshio_importer.py
+++ meshio_importer.py
@@ -1,13 +1,13 @@
-# bl_info = {
-#     "name": "MeshioImporterTool",
-#     "description": "Importer for meshio supported mesh files.",
-#     "author": "Hantao Hui",
-#     "version": (1, 0),
-#     "blender": (2, 90, 0),
-#     "warning": "",
-#     "support": "COMMUNITY",
-#     "category": "Import-Export",
-# }
+bl_info = {
+    "name": "MeshioImporterTool",
+    "description": "Importer for meshio supported mesh files.",
+    "author": "Hantao Hui",
+    "version": (1, 0),
+    "blender": (2, 90, 0),
+    "warning": "",
+    "support": "COMMUNITY",
+    "category": "Import-Export",
+}
 
 import bpy
 import bmesh
@@ -15,6 +15,8 @@ import numpy as np
 from bpy.app.handlers import persistent
 import meshio
 import fileseq
+import os
+import sys
 
 '''
 ====================Utility Functions=====================================
@@ -31,7 +33,6 @@ def show_message_box(message="", title="Message Box", icon="INFO"):
     bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
 
 def clear_screen():
-    import os
     os.system("cls")
 
 def check_type(fs):
@@ -41,74 +42,91 @@ def check_type(fs):
     elif mesh.cells[0].type == "triangle":
         return "mesh"
 
+# you can write your own color function here
+# input: attributes you want to use for coloring, 
+# the shape of input:  number of particles * dimension of attribute e.g. (6859,3) 6859 particles and each particle has a 3-d attribute 
+# output: np array of color information, the same length as number of particles, the value should be normalized to [0,1]
+def calculate_color(att_data:np.array) :
+    #  here is my implementation
+    if len(att_data.shape) >= 3:
+#         #  normally, this one shouldn't happen
+        show_message_box("attribute error: higher than 3 dimenion of attribute",icon="ERROR")
+
+    elif len(att_data.shape) == 2:
+        a, b = att_data.shape
+        res=np.zeros(a,dtype=np.float32)
+        res=np.linalg.norm(att_data, axis=1)
+        res/=np.max(res)
+        return res
+    elif len(att_data.shape) == 1:
+        res=np.copy(att_data)
+        res/=np.max(res)
+        return res
+
 '''
 ====================Importer Classes=====================================
 '''
 class particle_importer:
-    def __init__(self, fileseq, rotation= np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),emitter_obj_name=None,sphere_obj_name=None,material_name=None):
+    def __init__(self, fileseq, rotation= np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]]),emitter_obj_name=None,sphere_obj_name=None,material_name=None,tex_image_name=None,mesh_name=None):
 
         # self.path=path
         self.fileseq=fileseq
+        self.name=fileseq.basename()+"@"+fileseq.extension()
         self.rotation=rotation
         self.render_attributes = []  # all the possible attributes, and type
         self.used_render_attribute = None  # the attribute used for rendering
-        self.min_v = 0 # the min value of this attribute
-        self.max_v = 0 # the max value of this attribute
         self.emitterObject =None
         self.sphereObj = None
-        if not emitter_obj_name or not sphere_obj_name or not material_name:
+        if not emitter_obj_name or not sphere_obj_name or not material_name or not tex_image or not mesh_name:
             self.init_particles()
         else:
+            self.mesh=bpy.data.meshes[mesh_name]
             self.emitterObject=bpy.data.objects[emitter_obj_name]
             self.sphereObj=bpy.data.objects[sphere_obj_name]
             self.material=bpy.data.materials[material_name]
+            self.tex_image=bpy.data.images[tex_image_name]
   
     def init_particles(self):
-        # create emitter object
-        # bpy.ops.mesh.primitive_cube_add(enter_editmode=False, location=(0, 0, 0))
-        # self.emitterObject = bpy.context.active_object
-
-        self.mesh=bpy.data.meshes.new(name="test")
         try:
             meshio_mesh=meshio.read(self.fileseq[0])
         except:
-            print("file is missing : ",total_path)
+            show_message_box("file is missing : "+self.fileseq[0],icon="ERROR")
             return 
+
+        if meshio_mesh.point_data:
+            for k in meshio_mesh.point_data.keys():
+                self.render_attributes.append(k)
+        else:
+            show_message_box(
+                "no attributes avaible, all particles will be rendered as the same color"
+            )
+
         
+        self.mesh=bpy.data.meshes.new(name=self.name+"_mesh")
         mesh_vertices=meshio_mesh.points
-        vertices_count=len(meshio_mesh.points)
-        self.mesh.vertices.add(vertices_count)
+        self.particle_num = len(meshio_mesh.points)
+        
+        self.mesh.vertices.add(self.particle_num)
  
         pos=meshio_mesh.points @ self.rotation
 
         self.mesh.vertices.foreach_set("co",pos.ravel())
-        new_object = bpy.data.objects.new("test", self.mesh)
+        new_object = bpy.data.objects.new(self.name, self.mesh)
         bpy.data.collections[0].objects.link(new_object)
         self.emitterObject=new_object
-        # self.emitterObject.select_set(True)
+
         bpy.context.view_layer.objects.active = self.emitterObject
-        # bpy
 
-
-
-        self.emitterObject.hide_viewport = False
-        self.emitterObject.hide_render = False
-        self.emitterObject.hide_select = False
-        
-        # bpy.ops.object.modifier_add(type="PARTICLE_SYSTEM")
         bpy.ops.object.particle_system_add()
-        #  turn off the gravity
-        bpy.data.particles["ParticleSettings"].effector_weights.gravity = 0
-        # make the cube invincible
-        bpy.context.object.show_instancer_for_render = False
-        bpy.context.object.show_instancer_for_viewport = False
+
         # basic settings for the particles
         self.emitterObject.particle_systems[0].settings.frame_start = 0
+        self.emitterObject.particle_systems[0].settings.effector_weights.gravity = 0
         self.emitterObject.particle_systems[0].settings.frame_end = 0
         self.emitterObject.particle_systems[0].settings.lifetime = 1000
         self.emitterObject.particle_systems[0].settings.particle_size = 0.01
         self.emitterObject.particle_systems[0].settings.emit_from = 'VERT'
-        self.emitterObject.particle_systems[0].settings.count = vertices_count
+        self.emitterObject.particle_systems[0].settings.count = self.particle_num
         self.emitterObject.particle_systems[0].settings.use_emit_random = False
         self.emitterObject.particle_systems[0].settings.normal_factor = 0
 
@@ -122,10 +140,8 @@ class particle_importer:
         self.sphereObj.hide_render = True
         self.sphereObj.hide_select = True
         #  create new material
-        self.material = bpy.data.materials.new("particle_material")
+        self.material = bpy.data.materials.new(self.name+"particle_material")
         self.material.use_nodes = True
-
-        self.read_first_frame()
         self.init_materials()
 
         self.emitterObject.active_material = self.material
@@ -144,26 +160,18 @@ class particle_importer:
 
         output = nodes.new(type="ShaderNodeOutputMaterial")
         diffuse = nodes.new(type="ShaderNodeBsdfDiffuse")
-        
         particleInfo = nodes.new(type="ShaderNodeParticleInfo")
-
         math1 = nodes.new(type="ShaderNodeMath")
         math1.operation = "ADD"
         math1.inputs[1].default_value = 0.5
-
-
         math2 = nodes.new(type="ShaderNodeMath")
         math2.operation = "DIVIDE"
-        math2.inputs[1].default_value = self.particle_num # this should be the number of particles
-        
+        math2.inputs[1].default_value = self.particle_num # this should be the number of particles       
         combine=nodes.new(type="ShaderNodeCombineXYZ")
         combine.inputs[1].default_value = 0
         combine.inputs[2].default_value = 0
         tex=nodes.new(type="ShaderNodeTexImage")
         
-
-
-
         link = links.new(particleInfo.outputs["Index"], math1.inputs[0])
         link = links.new(math1.outputs["Value"], math2.inputs[0])
         link = links.new(math2.outputs["Value"], combine.inputs[0])
@@ -173,26 +181,6 @@ class particle_importer:
         self.tex_image=bpy.data.images.new('particle_tex_image',width=self.particle_num,height=1)
         tex.image=self.tex_image
 
-    def read_first_frame(self):
-        try:
-            mesh = meshio.read(
-                self.fileseq[0]
-            )
-        except Exception as e:
-            show_message_box("Can't read first frame file",icon="ERROR")
-            print(str(e))
-        if mesh.point_data:
-            for k in mesh.point_data.keys():
-                self.render_attributes.append(k)
-        else:
-            show_message_box(
-                "no attributes avaible, all particles will be rendered as the same color"
-            )
-
-        self.particle_num = len(mesh.points)
-
-
-
     def __call__(self, scene,depsgraph=None):
         frame_number = scene.frame_current
         frame_number = frame_number % len(self.fileseq) 
@@ -201,58 +189,30 @@ class particle_importer:
                 self.fileseq[frame_number]
             )
         except:
-            print("file: ", end="")
-            print(self.fileseq[frame_number])
-            print(" does not exist, this file will be skipped")
-            return
-
+            show_message_box("file is missing : "+self.fileseq[frame_number],icon="ERROR")
+            return 
 
         if len(mesh.points)!=self.particle_num: 
             self.particle_num=len(mesh.points)
             self.tex_image.generated_width=self.particle_num
+
         bm=bmesh.new()
         bm.from_mesh(self.mesh)
         bm.clear()    
         bm.to_mesh(self.mesh)
         bm.free()
         self.mesh.vertices.add(self.particle_num)
-        
- 
         pos=mesh.points @ self.rotation
-        
         self.mesh.vertices.foreach_set("co",pos.ravel())
-
 
         if self.used_render_attribute:
             att_str = self.used_render_attribute
             att_data = mesh.point_data[att_str]
-            if len(att_data.shape) >= 3:
-        #         #  normally, this one shouldn't happen
-                show_message_box("attribute error: higher than 3 dimenion of attribute",icon="ERROR")
-            elif len(att_data.shape) == 2:
-                a, b = att_data.shape
-                if b > 3:
-                    show_message_box(
-                        "attribute error: currently unsupport attributes more than 3 column",icon="ERROR"
-                    )
-                else:
-
-                    pixels = np.zeros((self.particle_num,4),dtype=np.float32)
-                    pixels[:,0] = np.linalg.norm(att_data, axis=1)
-
-                    max_v=np.max(np.linalg.norm(att_data, axis=1))
-                    pixels/=max_v
-                    pixels[:,3]=1
-                    self.tex_image.pixels.foreach_set( pixels.ravel())
-
-            elif len(att_data.shape) == 1:
-                pixels = np.zeros((self.particle_num, 4),dtype=np.float32)     
-                pixels[:, 0] = att_data
-                max_v = np.max(att_data)
-                pixels/=max_v
-                pixels[:,3]=1
-                self.tex_image.pixels.foreach_set( pixels.ravel())
-
+            color=calculate_color(att_data)
+            pixels=np.zeros((self.particle_num,4),dtype=np.float32)
+            pixels[:,0]=color
+            pixels[:,3]=1 
+            self.tex_image.pixels.foreach_set( pixels.ravel())
         else:
             pixels=[0] * 4*self.particle_num
             self.tex_image.pixels.foreach_set( pixels)
@@ -268,86 +228,36 @@ class particle_importer:
         if attribute_str in self.render_attributes:
             self.used_render_attribute = attribute_str
         else:
-            print(
+            show_message_box(
                 "attributes error: this attributs is not available in 1st frame of file"
             )
 
-    def get_minmax(self):
-        return self.min_v, self.max_v
-
     def clear(self):
         bpy.ops.object.select_all(action="DESELECT")
-        if self.emitterObject:
+        if self.emitterObject :
             self.emitterObject.select_set(True)
             bpy.ops.object.delete()
             self.emitterObject = None
+            bpy.data.meshes.remove(self.mesh)
+            self.mesh=None
         if self.sphereObj:
             bpy.data.meshes.remove(self.sphereObj.data)
-            # This doesn't work
-            # self.sphereObj.select_set(True)
-            # bpy.ops.object.delete()
             self.sphereObj = None
-
         for p in bpy.data.particles:
             if p.users == 0:
                 bpy.data.particles.remove(p)
-        for m in bpy.data.meshes:
-            if m.users == 0:
-                bpy.data.meshes.remove(m)
-        for m in bpy.data.materials:
-            if m.users == 0:
-                bpy.data.materials.remove(m)
+        if self.material:
+            bpy.data.materials.remove(self.material)
+            self.material=None
+        if self.tex_image:
+            bpy.data.images.remove(self.tex_image)
+            self.tex_image=None
 
     def __del__(self):
-        print("particles are removed")
         self.clear()
 
     def set_radius(self,r ):
         self.emitterObject.particle_systems[0].settings.particle_size = r
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class mesh_importer:
     def __init__(self,fileseq,rotation=np.array([[1,0,0],[0,0,1],[0,1,0]])):
@@ -361,8 +271,6 @@ class mesh_importer:
         self.v_col=None
         self.used_color_attribute= None
         self.init_mesh()
-        
-
 
     def create_face_data(self,meshio_cells):
         # todo: support other mesh structure, such as tetrahedron
@@ -380,8 +288,6 @@ class mesh_importer:
         vertices_count=len(meshio_mesh.points)
         mesh_faces=self.create_face_data(meshio_mesh.cells)
 
-        # if self.mesh:
-            # remove all the vertices from the mesh
         bm=bmesh.new()
         bm.from_mesh(self.mesh)
         bm.clear()    
@@ -436,7 +342,6 @@ class mesh_importer:
         for i, col in enumerate(v_col.data):
             col.color=mesh_colors[i*3],0,0,1
 
-
         self.mesh.update()
         self.mesh.validate()
 
@@ -461,11 +366,6 @@ class mesh_importer:
         bpy.data.collections[0].objects.link(new_object)
         self.obj=new_object
         self.obj.active_material=self.material
-
-
-
-
-
 
         total_path=self.fileseq[0]
         self.load_mesh(total_path)
@@ -662,15 +562,6 @@ class importer_properties(bpy.types.PropertyGroup):
     particle_material_name : bpy.props.StringProperty(name="Particle Material Obj Name")
 
 
-def update_min_max(self, context):
-    global importer
-    if not importer:
-        return
-    min_v, max_v = importer.get_minmax()
-    context.scene.my_tool.importer.min_value = min_v
-    context.scene.my_tool.importer.max_value = max_v
-
-
 class color_attribtue(bpy.types.PropertyGroup):
     name: bpy.props.StringProperty(name='color attr')
 
@@ -827,46 +718,6 @@ class MESHIO_IMPORT_PT_main_panel(bpy.types.Panel):
         # layout.operator("sequence.clear")
         layout.operator("test.test")
 
-
-# class PARTICLE_PT_panel(bpy.types.Panel):
-#     bl_label = "Particles Settings"
-#     bl_idname = "PARTICLE_PT_panel"
-#     bl_space_type = "VIEW_3D"
-#     bl_region_type = "UI"
-#     bl_category = "Meshio Importer"
-#     bl_parent_id = "MESHIO_IMPORT_PT_panel"
-
-#     def draw(self, context):
-#         t = context.scene.my_tool.importer.type
-#         if t != "particle":
-#             return
-#         else:
-#             scene = context.scene
-#             importer_prop = scene.my_tool.importer
-#             layout = self.layout
-#             layout.prop(importer_prop, "render")
-#             layout.prop(importer_prop, "min_value")
-#             layout.prop(importer_prop, "max_value")
-#             layout.prop(importer_prop, "particle_radius")
-
-
-
-# class MESH_PT_panel(bpy.types.Panel):
-#     bl_label = "Mesh Settings"
-#     bl_idname = "MESH_PT_panel"
-#     bl_space_type = "VIEW_3D"
-#     bl_region_type = "UI"
-#     bl_category = "Meshio Importer"
-#     bl_parent_id = "MESHIO_IMPORT_PT_panel"
-
-#     def draw(self, context):
-#         t = context.scene.my_tool.importer.type
-#         if t != "Mesh":
-#             pass
-#         else:
-#             print("todo: mesh settins")
-#             pass
-
 '''
 ====================Addon Operators=====================================
 '''
@@ -954,7 +805,6 @@ class meshio_loader_OT_load(bpy.types.Operator):
 
 
             bpy.app.handlers.frame_change_post.append(importer)
-            # bpy.app.handlers.frame_change_post.append(update_min_max)
 
         if importer_prop.type == "mesh":
             if importer:
@@ -996,20 +846,21 @@ classes = [
 
 @persistent
 def load_post(scene):
-    importer_prop = bpy.context.scene.my_tool.importer
-    global importer
-    if importer_prop.init:
-        fs=fileseq.findSequenceOnDisk(importer_prop.used_fs)
-        # if importer:
-            # bpy.ops.sequence.clear()
-        file_type=check_type(fs[0])
-        if file_type=='particle':
-            importer=particle_importer(fs,emitter_obj_name=importer_prop.particle_emitter_obj_name,sphere_obj_name=importer_prop.particle_sphere_obj_name,material_name=importer_prop.particle_material_name)
-            bpy.app.handlers.frame_change_post.append(importer)
-            bpy.app.handlers.frame_change_post.append(update_min_max)
-        elif file_type=='mesh':
-            importer=mesh_importer(fs)
-            bpy.app.handlers.frame_change_post.append(importer)
+    pass
+    # importer_prop = bpy.context.scene.my_tool.importer
+    # global importer
+    # if importer_prop.init:
+    #     fs=fileseq.findSequenceOnDisk(importer_prop.used_fs)
+    #     # if importer:
+    #         # bpy.ops.sequence.clear()
+    #     file_type=check_type(fs[0])
+    #     if file_type=='particle':
+    #         importer=particle_importer(fs,emitter_obj_name=importer_prop.particle_emitter_obj_name,sphere_obj_name=importer_prop.particle_sphere_obj_name,material_name=importer_prop.particle_material_name)
+    #         bpy.app.handlers.frame_change_post.append(importer)
+    #         bpy.app.handlers.frame_change_post.append(update_min_max)
+    #     elif file_type=='mesh':
+    #         importer=mesh_importer(fs)
+    #         bpy.app.handlers.frame_change_post.append(importer)
 
         
 
