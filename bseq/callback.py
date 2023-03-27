a81d7b677b647cf45613572c71a4189cc69fafe6
julius.stotz <julius.stotz@rwth-aachen.de>
2023-03-28 00:07:19 +0200

    Updated comment

--- bseq/callback.py
+++ bseq/callback.py
@@ -11,6 +11,10 @@ def update_path(self, context):
     context.scene.BSEQ.use_pattern = False
     context.scene.BSEQ.pattern = ""
 
+    '''
+    Detects all the file sequences in the directory
+    '''
+    
     p = context.scene.BSEQ.path
     try:
         f = fileseq.findSequencesOnDisk(p)
@@ -31,9 +35,6 @@ def update_path(self, context):
 
 
 def item_fileseq(self, context):
-    '''
-    Detects all the file sequences in the directory
-    '''
     return file_sequences
 
 
