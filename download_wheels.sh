d0ffb8a4afd3b496ca3c755e1244d181a39ab174
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2024-11-11 17:21:56 +0100

    add blender manifest for 4.2 marketplace support

---
+++ download_wheels.sh
@@ -0,0 +1,4 @@
+pip wheel fileseq==1.15.2 -w ./wheels --no-deps
+pip wheel meshio==5.3.4 -w ./wheels --no-deps
+pip wheel future==0.18.3 -w ./wheels --no-deps
+pip wheel rich==13.7.0 -w ./wheels --no-deps
