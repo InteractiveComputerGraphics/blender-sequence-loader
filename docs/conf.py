daedd1fec72ca532ed0fc09c1cfe3c10be942f5d
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2024-11-20 11:11:23 +0100

    Update version in docs conf.py (#37)

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'blender-sequence-loader'
 copyright = '2024, InteractiveComputerGraphics'
 author = 'InteractiveComputerGraphics'
-release = '0.3.2'
+release = '0.3.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
