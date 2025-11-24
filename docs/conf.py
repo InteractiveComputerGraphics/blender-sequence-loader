970d9a7150cd338911dc1fb617bb476b8734c4cc
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2025-11-24 14:10:46 +0100

    increase version since I cant re-upload with the same version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Sequence Loader'
 copyright = '2025, InteractiveComputerGraphics'
 author = 'InteractiveComputerGraphics'
-release = '0.3.7'
+release = '0.3.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
