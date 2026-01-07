707a25a3932a22a01e3178aa9340d63a8bdd7863
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2026-01-07 15:25:25 +0100

    bump version for new release

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Sequence Loader'
 copyright = '2025, InteractiveComputerGraphics'
 author = 'InteractiveComputerGraphics'
-release = '0.3.9'
+release = '0.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
